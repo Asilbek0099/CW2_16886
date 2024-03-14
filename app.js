da6a5fbe22980a5163bf4a8eafe690102a14267d
Asilbek0099 <156507579+Asilbek0099@users.noreply.github.com>
2024-03-14 21:58:21 +0500

    added and updated the buttons and some functions

--- app.js
+++ app.js
@@ -1,62 +1,83 @@
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+const url = require('url');
 
-const express = require("express");
-const app = express();
 const port = 3000;
 
-const id = require("uniqid");
+const server = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true);
+  const reqPath = reqUrl.pathname;
 
-app.set("view engine", "pug");
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  let created = req.query.create;
-
-  let event = getAll("event");
-  if (created) {
-    res.render("list", { created: true, event: event });
-  } else {
-    res.render("list", { created: false, event: event });
+  // Serve static files
+  if (reqPath.startsWith('/public/')) {
+    const filePath = path.join(__dirname, reqPath);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        const contentType = getContentType(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+      }
+    });
+    return;
   }
-  res.render("list", {});
+
+  if (reqPath === '/') {
+    const created = reqUrl.query.created === 'true';
+    const events = getAll("events");
+    renderTemplate(res, 'index', { created, events });
+  } else if (reqPath === '/list') {
+    const created = reqUrl.query.created === 'true';
+    const events = getAll("events");
+    renderTemplate(res, 'list', { created, events });
+  } else if (reqPath === '/create') {
+    renderTemplate(res, 'create');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
 });
 
-app.get("/create", (req, res) => {
-  res.render("create", {});
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
-app.post("/create", (req, res) => {
-  let data = req.body;
-
-  let events = {
-    id: id(),
-    Name: data.NameOfEvent,
-    Location: data.Location,
-    Description: data.Description,
-    Purpose: data.Purpose,
-    event_date: data.event_date,
-  };
-
-  let event = getAll("event");
-
-  event.push(events);
-
-  writeAll("event", event);
-
-  res.redirect("/?created=true");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-function getAll(filename) {
-  return JSON.parse(fs.readFileSync(`./App/${filename}.json`));
+function getAll() {
+  try {
+    const data = fs.readFileSync(`./data/event.json`, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return [];
+  }
 }
 
-function writeAll(filename, data) {
-  return fs.writeFileSync(`./App/${filename}.json`, JSON.stringify(data));
+// Function to render a Pug template
+function renderTemplate(res, templateName, data = {}) {
+  const templatePath = path.join(__dirname, 'views', `${templateName}.pug`);
+  fs.readFile(templatePath, 'utf8', (err, templateContent) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    } else {
+      const renderedTemplate = pug.render(templateContent, data);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(renderedTemplate);
+    }
+  });
+}
+
+// Function to get content type based on file extension
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.css':
+      return 'text/css';
+    default:
+      return 'text/plain';
+  }
 }
